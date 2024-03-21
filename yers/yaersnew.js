6b254ad52b90ccb64479df60cf3b050695f30cf6
kamyar <kami6535@gmail.com>
2024-03-21 11:56:21 +0330

    time

--- yers/yaersnew.js
+++ yers/yaersnew.js
@@ -1,6 +1,15 @@
-const you => {
-    
+const oneDay = 24 * 60 * 60 * 1000;
+const oneHour = 60 * 60 * 1000;
+const oneMinute = 60 * 1000;
+const oneSecond = 1000;
+/!haha tame/;
 
+const counter = () => {
+  const newyYearDate = new Date("1jan 2025").getTime();
+  const CurrantDate = new Date().getTime();
+  const diffrentTime = newyYearDate - CurrantDate;
+  const Days = Math.floor(diffrentTime / oneDay);
+};
 
-
-}
+/!    تایم /;
+const setInterval = (counter, 1000);

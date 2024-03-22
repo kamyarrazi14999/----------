a6080fcf8835a035637b1cb9e34e027135178d19
kamyar <kami6535@gmail.com>
2024-03-22 13:53:38 +0330

    time change script

--- yers/yaersnew.js
+++ yers/yaersnew.js
@@ -1,15 +1,21 @@
 const oneDay = 24 * 60 * 60 * 1000;
 const oneHour = 60 * 60 * 1000;
 const oneMinute = 60 * 1000;
-const oneSecond = 1000;
 /!haha tame/;
 
 const counter = () => {
-  const newyYearDate = new Date("1jan 2025").getTime();
-  const CurrantDate = new Date().getTime();
-  const diffrentTime = newyYearDate - CurrantDate;
-  const Days = Math.floor(diffrentTime / oneDay);
-};
+  const newYearDate = new Date("1 jan 2025").getTime();
+  const currnentDate = new Date().getTime();
+  const diffrentData = newYearDate - currnentDate;
+  const days = Math.floor(diffrentData / oneDay);
+  const hours = Math.floor((diffrentData % oneDay) / oneHour);
+  const minutes = Math.floor((diffrentData % oneHour) / oneMinute);
+  const seconds = Math.floor((diffrentData % oneMinute) / 1000);
 
-/!    تایم /;
-const setInterval = (counter, 1000);
+  const valuse = [days, hours, minutes, seconds];
+  const deadlineMants = document.querySelectorAll(".dead-line-number");
+  deadlineMants.forEach((item , index)=>{
+    item.innerHTML = valuse[index];
+});
+};
+const interval = setInterval(counter, 1000);

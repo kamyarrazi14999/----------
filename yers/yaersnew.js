8e6c609d1d46e9106dc5211a97eca509ba4b7b0d
kamyar <kami6535@gmail.com>
2024-03-24 13:30:38 +0330

    time set

--- yers/yaersnew.js
+++ yers/yaersnew.js
@@ -4,7 +4,7 @@ const oneMinute = 60 * 1000;
 /!haha tame/;
 
 const counter = () => {
-  const newYearDate = new Date("1 jan 2025").getTime();
+  const newYearDate = new Date("1 jan 2024").getTime();
   const currnentDate = new Date().getTime();
   const diffrentData = newYearDate - currnentDate;
   const days = Math.floor(diffrentData / oneDay);
@@ -14,8 +14,24 @@ const counter = () => {
 
   const valuse = [days, hours, minutes, seconds];
   const deadlineMants = document.querySelectorAll(".dead-line-number");
-  deadlineMants.forEach((item , index)=>{
-    item.innerHTML = valuse[index];
-});
+  deadlineMants.forEach((item, index) => {
+    item.innerHTML =  formatNuber(valuse[index]);
+  });
+  if (diffrentData <= 0) {
+    const newYEARbox = document.querySelector(".new-year-1556 ");
+    newYEARbox.innerHTML = `<img src="./giphy.webp" alt="new years"> 
+    <audio controls    src="./fireworks-50524.mp3   "></audio>  `;
+    clearInterval(interval);
+  }
 };
+const formatNuber = (Number) => {
+  if(Number<10){
+    return `0${Number}`
+  } else {
+    return Number
+  }
+
+
+
+}
 const interval = setInterval(counter, 1000);
